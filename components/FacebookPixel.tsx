a1bf07c2806d4628e813cb7fde40df2f27586b2a
mustaqimbd <mustaqimkhanbd@gmail.com>
2026-09-08 11:52:07 +0600

    fix: update meta pixel and add noscript fallback, disable autoConfig

--- components/FacebookPixel.tsx
+++ components/FacebookPixel.tsx
@@ -38,11 +38,21 @@ export default function FacebookPixel() {
             t.src=v;s=b.getElementsByTagName(e)[0];
             s.parentNode.insertBefore(t,s)}(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('set', 'autoConfig', false, '${process.env.NEXT_PUBLIC_META_PIXEL_ID}');
             fbq('init', '${process.env.NEXT_PUBLIC_META_PIXEL_ID}');
             fbq('track', 'PageView');
           `,
         }}
       />
+      <noscript>
+        <img
+          height="1"
+          width="1"
+          style={{ display: "none" }}
+          src={`https://www.facebook.com/tr?id=${process.env.NEXT_PUBLIC_META_PIXEL_ID}&ev=PageView&noscript=1`}
+          alt=""
+        />
+      </noscript>
     </>
   );
 }
