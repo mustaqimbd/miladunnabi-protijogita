0ecf51d67bd493c512c9e0c79ffe6784c9691665
mustaqimbd <mustaqimkhanbd@gmail.com>
2026-09-20 16:16:27 +0600

    chore: add caching headers for static assets to reduce edge requests

--- next.config.ts
+++ next.config.ts
@@ -12,6 +12,15 @@ const nextConfig: NextConfig = {
           },
         ],
       },
+      {
+        source: "/(.*\\.(?:jpg|jpeg|gif|png|svg|ico|webp))",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
     ];
   },
 };
